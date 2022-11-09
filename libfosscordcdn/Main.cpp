95981f7c3ac38829c482c0a06fbb9a2c0b29165d
Puyodead1 <puyodead@protonmail.com>
2022-11-08 21:12:49 -0500

    v2 - use buffer

--- libfosscordcdn/Main.cpp
+++ libfosscordcdn/Main.cpp
@@ -2,9 +2,9 @@
 
 #include <stdio.h>
 #include <node.h>
-
-#include <node.h>
 #include <node_object_wrap.h>
+#include <node_buffer.h>
+#include <windows.h>
 
 extern "C"
 {
@@ -15,19 +15,58 @@ extern "C"
 
 using namespace v8;
 
-int FFMPEGGetDimensions(Isolate* isolate, char* path, int* width, int* height)
+
+struct buffer_data {
+    uint8_t* ptr;
+    size_t size;
+};
+
+static int read_packet(void* opaque, uint8_t* buf, int buf_size)
+{
+    struct buffer_data* bd = (struct buffer_data*)opaque;
+    buf_size = FFMIN(buf_size, bd->size);
+    memcpy(buf, bd->ptr, buf_size);
+    bd->ptr += buf_size;
+    bd->size -= buf_size;
+    return buf_size;
+}
+
+
+int FFMPEGGetDimensions(Isolate* isolate, uint8_t* buf, int bufLen, int* width, int* height)
 {
     AVFormatContext* pFormatCtx = NULL;
+    AVIOContext* pAvioCtx = NULL;
     const AVCodecParameters* pCodecParams = NULL;
     int ret;
     int videoStream = -1;
+    struct buffer_data bd = { 0 };
+    uint8_t* avio_ctx_buffer = NULL;
+    size_t avio_ctx_buffer_size = 4096;
 
-    if ((ret = avformat_open_input(&pFormatCtx, path, NULL, NULL)))
-        return ret;
+    bd.ptr = buf;
+    bd.size = bufLen;
 
-    if ((ret = avformat_find_stream_info(pFormatCtx, NULL)) < 0) {
-        isolate->ThrowException(v8::Exception::Error(String::NewFromUtf8(isolate, "Cannot find stream information").ToLocalChecked()));
-        return ret;
+    if (!(pFormatCtx = avformat_alloc_context())) {
+        ret = AVERROR(ENOMEM);
+        goto end;
+    }
+
+    avio_ctx_buffer = (uint8_t*)av_malloc(avio_ctx_buffer_size);
+    if (!avio_ctx_buffer) {
+        ret = AVERROR(ENOMEM);
+        goto end;
+    }
+    pAvioCtx = avio_alloc_context(avio_ctx_buffer, avio_ctx_buffer_size,
+        0, &bd, &read_packet, NULL, NULL);
+    if (!pAvioCtx) {
+        ret = AVERROR(ENOMEM);
+        goto end;
+    }
+    pFormatCtx->pb = pAvioCtx;
+    ret = avformat_open_input(&pFormatCtx, NULL, NULL, NULL);
+    if (ret < 0) {
+        isolate->ThrowException(v8::Exception::Error(String::NewFromUtf8(isolate, "Failed to open input").ToLocalChecked()));
+        goto end;
     }
 
     for (int i = 0; i < pFormatCtx->nb_streams; i++)
@@ -41,7 +80,7 @@ int FFMPEGGetDimensions(Isolate* isolate, char* path, int* width, int* height)
     if (videoStream == -1)
     {
         isolate->ThrowException(v8::Exception::Error(String::NewFromUtf8(isolate, "Cannot find video stream").ToLocalChecked()));
-        return 1;
+        goto end;
     }
 
     pCodecParams = pFormatCtx->streams[videoStream]->codecpar;
@@ -49,7 +88,16 @@ int FFMPEGGetDimensions(Isolate* isolate, char* path, int* width, int* height)
     *width = pCodecParams->width;
     *height = pCodecParams->height;
 
+
+end:
     avformat_close_input(&pFormatCtx);
+    av_freep(&pAvioCtx->buffer);
+    av_freep(&pAvioCtx);
+    if (ret < 0) {
+        isolate->ThrowException(v8::Exception::Error(String::NewFromUtf8(isolate, "something happened while cleaning up").ToLocalChecked()));
+        return 1;
+    }
+
     return 0;
 }
 
@@ -58,10 +106,10 @@ void GetDimensions(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
     Local<Context> context = isolate->GetCurrentContext();
 
-    Local<String> arg = args[0]->ToString(context).ToLocalChecked();
-    v8::String::Utf8Value path(isolate, arg);
+    uint8_t* buf = (uint8_t*)node::Buffer::Data(args[0]->ToObject(context).ToLocalChecked());
+    int bufSize = args[1]->Int32Value(context).ToChecked();
 
-    int ret = FFMPEGGetDimensions(isolate, *path, &width, &height);
+    int ret = FFMPEGGetDimensions(isolate, buf, bufSize, &width, &height);
 
     Local<Object> retObj = Object::New(isolate);
     retObj->Set(context,
